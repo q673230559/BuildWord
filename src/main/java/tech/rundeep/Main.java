1fdabcfe069e7e3875f1ed1f9155c2ce1a4f4f8b
Administrator <451367682@qq.com>
2017-07-03 10:43:49 +0800

    .

---
+++ src/main/java/tech/rundeep/Main.java
@@ -0,0 +1,10 @@
+package tech.rundeep;
+
+/**
+ * Created by Administrator on 2017/7/3.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
