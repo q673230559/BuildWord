8057099e3375e9fd5063c887d55104602b6b5ceb
Administrator <451367682@qq.com>
2017-07-03 17:40:27 +0800

    .

--- src/main/java/tech/rundeep/Main.java
+++ src/main/java/tech/rundeep/Main.java
@@ -1,10 +1,36 @@
 package tech.rundeep;
 
+import org.apache.poi.xwpf.extractor.XWPFWordExtractor;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
 /**
  * Created by Administrator on 2017/7/3.
+ * reference: http://blog.csdn.net/tuzongxun/article/details/51992704
  */
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Hello");
+        readAndWriterTest4();
     }
+
+    public static void readAndWriterTest4() throws IOException {
+        File file = new File("D:\\02 项目需求规格文档模板.docx");
+        String str = "";
+        try {
+            FileInputStream fis = new FileInputStream(file);
+            XWPFDocument xdoc = new XWPFDocument(fis);
+            XWPFWordExtractor extractor = new XWPFWordExtractor(xdoc);
+            String doc1 = extractor.getText();
+            System.out.println(doc1);
+            fis.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
